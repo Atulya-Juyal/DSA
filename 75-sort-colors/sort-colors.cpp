16b5fbbb79e51e8bb2e0f66c7291819494895eb8
Atulya Juyal <144871382+Atulya-Juyal@users.noreply.github.com>
2026-01-31 13:04:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (47.10%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for(int i : nums) mp[i]++;
+        // unordered_map<int, int> mp;
+        // for(int i : nums) mp[i]++;
 
-        int it = 0;
-        int n = mp[0];
-        for(int i = it; i < n; i++) nums[i] = 0;
+        // int it = 0;
+        // int n = mp[0];
+        // for(int i = it; i < n; i++) nums[i] = 0;
 
-        it += mp[0];
-        n += mp[1];
-        for(int i = it; i < n; i++) nums[i] = 1;
+        // it += mp[0];
+        // n += mp[1];
+        // for(int i = it; i < n; i++) nums[i] = 1;
 
-        it += mp[1];
-        n += mp[2];
-        for(int i = it; i < n; i++) nums[i] = 2;
+        // it += mp[1];
+        // n += mp[2];
+        // for(int i = it; i < n; i++) nums[i] = 2;
 
-        return;
+        // return;
+
+        int l = 0;
+        int m = 0;
+        int h = nums.size()-1;
+
+        while(m <= h){
+            if(nums[m] == 0){
+                swap(nums[l], nums[m]);
+                l++;
+                m++;
+            }
+
+            else if(nums[m] == 1) m++;
+
+            else{
+                swap(nums[m], nums[h]);
+                h--;
+            }
+        }
     }
 };
