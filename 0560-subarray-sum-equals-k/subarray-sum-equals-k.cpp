c256edd774877a1a41cadcb376e842892f2a1371
Atulya Juyal <144871382+Atulya-Juyal@users.noreply.github.com>
2026-02-01 19:11:46 +0530

    Time: 34 ms (89.15%) | Memory: 45.6 MB (37.02%) - LeetSync

---
+++ 0560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int pre = 0;
+        int cnt = 0;
+
+        unordered_map<int,int> mp;
+        mp[0] = 1;
+
+        for(int i = 0; i < nums.size(); i++){
+            pre += nums[i];
+
+            if(mp.find(pre-k) != mp.end()){
+                cnt += mp[pre-k];
+            }
+
+            mp[pre]++;
+        }
+
+        return cnt;
+    }
+};
