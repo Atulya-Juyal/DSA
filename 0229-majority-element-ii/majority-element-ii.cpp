decaf3e6e72426d8db72ce09a1c0db210cc24f5c
Atulya Juyal <144871382+Atulya-Juyal@users.noreply.github.com>
2026-05-13 15:29:36 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (63.26%) - LeetSync

--- 0229-majority-element-ii/majority-element-ii.cpp
+++ 0229-majority-element-ii/majority-element-ii.cpp
@@ -1,44 +1,44 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int c1 = 0;
+        int c2 = 0;
+
         int e1 = INT_MIN;
         int e2 = INT_MIN;
-        int cnt1 = 0;
-        int cnt2 = 0;
-
-        for(int i = 0; i < nums.size(); i++){
-            if(cnt1 == 0 && nums[i] != e2){
-                e1 = nums[i];
-                cnt1++;
-            }
-
-            else if(cnt2 == 0 && nums[i] != e1){
-                e2 = nums[i];
-                cnt2++;
-            }
-
-            else if(e1 == nums[i]) cnt1++;
-
-            else if(e2 == nums[i]) cnt2++;
-
-            else{
-                cnt1--;
-                cnt2--;
-            }
-        }
-
-        cnt1 = 0;
-        cnt2 = 0;
 
         for(int i : nums){
-            if(i == e1) cnt1++;
-            else if(i == e2) cnt2++;
+            if(c1 == 0 && i != e2){
+                e1 = i;
+                c1++;
+            }
+
+            else if(c2 == 0 && i != e1){
+                e2 = i;
+                c2++;
+            }
+
+            else if(i == e1) c1++;
+            else if(i == e2) c2++;
+
+            else{
+                c1--;
+                c2--;
+            }
         }
 
-        if(cnt1 > floor(nums.size()/3) && cnt2 > floor(nums.size()/3)) return {e1, e2};
-        else if(cnt1 > floor(nums.size()/3)) return {e1};
-        else if(cnt2 > floor(nums.size()/3)) return {e2};
-        else return {};
-        
+        c1 = 0;
+        c2 = 0;
+
+        for(int i : nums){
+            if(i == e1) c1++;
+            else if(i == e2) c2++;
+        }
+
+        vector<int> res;
+        if(c1 > floor(nums.size()/3)) res.push_back(e1);
+        if(c2 > floor(nums.size()/3)) res.push_back(e2);
+
+        return res;
     }
 };
