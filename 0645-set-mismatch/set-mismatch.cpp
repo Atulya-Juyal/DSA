246bd5dba608e5d04bf8a0c7f98c757806510551
Atulya Juyal <144871382+Atulya-Juyal@users.noreply.github.com>
2026-05-16 01:20:45 +0530

    Time: 0 ms (100.00%) | Memory: 25.3 MB (63.06%) - LeetSync

--- 0645-set-mismatch/set-mismatch.cpp
+++ 0645-set-mismatch/set-mismatch.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n = nums.size();
-        int r = 0;
+        long long n = nums.size();
+        long long s = 0;
+        long long sn = 0;
+        long long v1 = 0;
+        long long v2 = 0;
 
-        for(int i = 0; i < n; i++){
-            r ^= nums[i];
-            r ^= i+1;
-        }
+        for(int i : nums) s += i;
+        sn = (n*(n+1))/2;
+        v1 = s - sn;
 
-        int bitNo = 0;
-        while(true){
-            if((r & (1 << bitNo)) != 0) break; 
-            bitNo++;
-        }
+        s = 0;
+        sn = 0;
+        for(int i : nums) s += i*i;
+        sn = (n*(n+1)*(2*n+1))/6;
+        v2 = s - sn;
+        v2 = v2 / v1;
 
-        int z = 0;
-        int o = 0;
-        for(int i : nums){
-            if((i & (1 << bitNo)) == 0) z ^= i;
-            else o ^= i;
-        }
-        for(int i = 1; i <= n; i++){
-            if((i & (1 << bitNo)) == 0) z ^= i;
-            else o ^= i;
-        }
+        int x = (v1+v2)/2;
+        int y = v2 - x;
 
-        int cnt = 0;
-        for(int i : nums){
-            if(z == i) return {z, o};
-        }
-        return {o, z};
+        return {x, y};
     }
 };
