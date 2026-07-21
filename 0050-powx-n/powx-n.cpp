d91ddf30bace578e4c796746257efad4ba10ab42
Atulya Juyal <144871382+Atulya-Juyal@users.noreply.github.com>
2026-07-22 00:21:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.70%) - LeetSync

---
+++ 0050-powx-n/powx-n.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == INT_MIN) return 1.0 / x * myPow(x, n+1);
+        if(n < 0) return 1.0 / myPow(x, -n);
+
+        if(n == 0) return 1.0;
+        if(n == 1) return x;
+
+        if(n % 2 == 0) return myPow(x*x, n/2);
+        return x * myPow(x, n-1);
+    }
+};
