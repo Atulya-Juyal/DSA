7fe500f5e842feea695292954cdce788cbba45b7
Atulya Juyal <144871382+Atulya-Juyal@users.noreply.github.com>
2026-07-21 10:29:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (33.14%) - LeetSync

--- 0392-is-subsequence/is-subsequence.cpp
+++ 0392-is-subsequence/is-subsequence.cpp
@@ -10,12 +10,11 @@ public:
         int i = 0;
         int j = 0;
 
-        while(j <= m){
-            if(i == n) return true;
-
+        while(j < m){            
             if(s[i] == t[j]) i++;
             j++;
         }
+        if(i == n) return true;
 
         return false;
     }
