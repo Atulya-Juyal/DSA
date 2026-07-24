7b2aea70b3ab79a73e575ba311d820d9a8475b1a
Atulya Juyal <144871382+Atulya-Juyal@users.noreply.github.com>
2026-07-24 05:41:58 +0530

    Time: 3 ms (61.78%) | Memory: 10.3 MB (49.28%) - LeetSync

--- 0051-n-queens/n-queens.cpp
+++ 0051-n-queens/n-queens.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
-    bool isSafe(vector<string>& board, int row, int col, int n){
-        for(int j = 0; j < col; j++){
-            if(board[row][j] == 'Q') return false;
-        }
+    // bool isSafe(vector<string>& board, int row, int col, int n){
+    //     for(int j = 0; j < col; j++){
+    //         if(board[row][j] == 'Q') return false;
+    //     }
 
-        for(int i = row-1, j = col-1; i >= 0 && j >= 0; i-- , j--){
-            if(board[i][j] == 'Q') return false;
-        }
+    //     for(int i = row-1, j = col-1; i >= 0 && j >= 0; i-- , j--){
+    //         if(board[i][j] == 'Q') return false;
+    //     }
 
-        for(int i = row+1, j = col-1; i < n && j >= 0; i++ , j--){
-            if(board[i][j] == 'Q') return false;
-        }
+    //     for(int i = row+1, j = col-1; i < n && j >= 0; i++ , j--){
+    //         if(board[i][j] == 'Q') return false;
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
 
 
-    void solve(vector<vector<string>>& res, vector<string>& board, int col, int n){
+    void solve(vector<vector<string>>& res, vector<string>& board, int col, int n, vector<int>& left, vector<int>& lowerLeft, vector<int>& upperLeft){
         if(col == n){
             res.push_back(board);
             return;
         }
 
         for(int row = 0; row < n; row++){
-            if(isSafe(board, row, col, n)){
+            if(left[row] == 0 && lowerLeft[col+row] == 0 && upperLeft[n-1 + col-row] == 0){
+                left[row] = 1;
+                lowerLeft[col+row] = 1;
+                upperLeft[n-1 + col-row] = 1;
+
                 board[row][col] = 'Q';
-                solve(res, board, col+1, n);
+
+                solve(res, board, col+1, n, left, lowerLeft, upperLeft);
+
+                left[row] = 0;
+                lowerLeft[col+row] = 0;
+                upperLeft[n-1 + col-row] = 0;
+
                 board[row][col] = '.';
             }
         }
@@ -38,7 +48,12 @@ public:
         string s(n, '.');
         vector<string> board(n, s);
 
-        solve(res, board, 0, n);
+        vector<int> left(n, 0);
+        vector<int> lowerLeft(2*n-1, 0);
+        vector<int> upperLeft(2*n-1, 0);
+        
+
+        solve(res, board, 0, n, left, lowerLeft, upperLeft);
 
         return res;
     }
