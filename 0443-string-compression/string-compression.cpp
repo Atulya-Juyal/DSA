32857dcce1a4d86c0d67774916881a1061cd2070
Atulya Juyal <144871382+Atulya-Juyal@users.noreply.github.com>
2026-08-04 00:51:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (76.34%) - LeetSync

--- 0443-string-compression/string-compression.cpp
+++ 0443-string-compression/string-compression.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int compress(vector<char>& chars) {
         int i = 0;
+        int j = 0;
         int k = 0;
 
         while(i < chars.size()) {
             char c = chars[i];
-            int j = i;
-
+            
             while(j < chars.size() && chars[j] == c) j++;
 
             chars[k] = c;
